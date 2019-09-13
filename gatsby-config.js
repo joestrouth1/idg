c421ebe93b2a263fd3344884170dd49dd26010d0
Joe Strouth <joestrouth@curo.com>
2019-09-13 09:56:08 -0500

    Remove storybook

--- gatsby-config.js
+++ gatsby-config.js
@@ -84,7 +84,7 @@ module.exports = {
       resolve: `gatsby-plugin-nprogress`,
       options: {
         // Setting a color is optional.
-        // color: `tomato`,
+        color: `#0A7E76`,
         // Disable the loading spinner.
         showSpinner: false,
       },
